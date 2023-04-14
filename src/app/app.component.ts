3069a2359f55a3c1f9e6849e2490fe5d43b79b59
pratikshaGorgal <119149138+pratikshaGorgal@users.noreply.github.com>
2023-04-14 22:19:07 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public Data="_____"
+  public fun()
+  {
+    this.Data="Educating for better tomorrow";
+  }
+}
